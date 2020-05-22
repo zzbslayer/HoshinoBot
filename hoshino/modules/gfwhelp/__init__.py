fd37f64a8a737004634968be007b3450c293e5a1
王见思 <5361064+zzbslayer@user.noreply.gitee.com>
2020-05-22 15:37:17 +0800

    v2ray help

---
+++ hoshino/modules/gfwhelp/__init__.py
@@ -0,0 +1,44 @@
+from hoshino import Service, Privilege
+
+sv = Service('gfw-help', manage_priv=Privilege.SUPERUSER, enable_on_default=False, visible=False)
+help_message = '''
+=====================
+-      使用说明      -
+=====================
+输入方括号[]内的关键词即可触发相应的功能
+※部分功能必须手动at本bot才会触发(复制无效)
+=====================
+-      General      -
+=====================
+[演唱会帮助] 演唱会使用说明
+[演唱会模式] 演唱会模式说明
+[演唱会门票] 演唱会门票说明
+=====================
+-     v2 演唱会      -
+=====================
+[v2-how] 如何参与 v2 演唱会
+[v2-download] v2 演唱会门票下载
+[v2-config] v2 演唱会门票配置信息
+'''
+
+help_aliases=('演唱会帮助', '演唱会说明', '演唱会help', '演唱会 help')
+@sv.on_command('gfw-help', aliases=help_aliases)
+async def gfw_help(session):
+    await session.send(help_message)
+
+mode_aliases=('演唱会模式', '演唱会mode', '演唱会 mode')
+@sv.on_command('proxy-mode', aliases=mode_aliases)
+async def mode_help(session):
+    msg = '演唱会模式'
+    msg = f'{msg}\n全局：全部歌曲从演唱会听'
+    msg = f'{msg}\nPAC：特定歌曲从演唱会听'
+    msg = f'{msg}\n直连：不从演唱会听歌'
+    await session.send(msg)
+
+tickets = ('ss', 'v2')
+mode_aliases=('演唱会门票')
+@sv.on_command('proxy-client', aliases=mode_aliases)
+async def mode_help(session):
+    msg = '演唱会门票十元每月，喜欢的粉丝们可以酌情打赏'
+    msg = f'{msg}\n目前支持 {tickets}'
+    await session.send(msg)
---
+++ hoshino/modules/gfwhelp/v2ray.py
@@ -0,0 +1,37 @@
+from hoshino import Service, Privilege
+
+sv = Service('v2ray-help', manage_priv=Privilege.SUPERUSER, enable_on_default=False, visible=False)
+
+windows_download = 'http://47.106.8.44/gfw/v2/v2ray-windows-64.zip'
+ios_download = '请在外区商店购买 shadowrocket'
+v2download_aliases=('v2download', 'v2raydownload', 'v2下载', 'v2ray下载')
+@sv.on_command('v2-download', aliases=v2download_aliases, only_to_me=False)
+async def v2download(session):
+    msg = '下载地址：'
+    msg = f'{msg}\nwindows: {windows_download}'
+    msg = f'{msg}\nios: {ios_download}'
+    msg = f'{msg}\n如果没有您使用的系统，不如问问万能的群友'
+    await session.send(msg)
+
+addresses = ['zenyatta.doomfist.xyz', 'ana.doomfist.xyz']
+port = 20900
+uuid = '034cb94e-7f10-47e8-a020-fca628f0a94a'
+v2config_aliases=('v2config', 'v2rayconfig', 'v2配置', 'v2ray配置')
+@sv.on_command('v2-config', aliases=v2config_aliases, only_to_me=False)
+async def v2download(session):
+    msg = '配置方法：'
+    msg = f'{msg}\n地址(address): {addresses} （任选一个即可，如果其中某个地址连接有问题，请更换列表中其他地址）'
+    msg = f'{msg}\n端口(port): {port}'
+    msg = f'{msg}\n用户ID(id/uuid): {uuid}'
+    msg = f'{msg}\n其他信息保持默认配置即可'
+    await session.send(msg)
+
+v2config_aliases=('v2how', 'v2rayhow')
+@sv.on_command('v2-how', aliases=v2config_aliases, only_to_me=False)
+async def v2download(session):
+    msg = '使用方法：'
+    msg = f'{msg}\n1. 下载 v2 客户端'
+    msg = f'{msg}\n2. 配置 v2 客户端'
+    msg = f'{msg}\n3. 启动 v2'
+    msg = f'{msg}\n其他信息保持默认配置即可'
+    await session.send(msg)
