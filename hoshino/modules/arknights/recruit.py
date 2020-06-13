097d03358f6314af73b2e3cfa207030ea75d2cb8
王见思 <5361064+zzbslayer@user.noreply.gitee.com>
2020-06-13 19:54:32 +0800

    Fix bilibili wiki bug locally

--- hoshino/modules/arknights/recruit.py
+++ hoshino/modules/arknights/recruit.py
@@ -124,7 +124,7 @@ async def _async_init():
 
 asyncio.get_event_loop().run_until_complete(_async_init())
 
-HELP_MESSAGE = '请输入标签。默认仅显示必得4★的TAG组合\n示例：公开招募 位移 近战位\n显示所有可能干员请使用参数"-a"或"全部"\n示例：公开招募 -a 位移 近战位'
+HELP_MESSAGE = '请输入标签。默认仅显示必得4★的TAG组合\n示例：公开招募 位移 近战位\n显示所有可能干员请使用参数"-a"或"全部"\n示例：公开招募 -a 位移 近战位\n网页版：https://www.bigfun.cn/tools/aktools-old/akhr.html'
 @sv.on_command('ark-recruit', aliases=('公开招募', "公招"), only_to_me=False)
 async def public_recruit(session):
     over4_only = True
